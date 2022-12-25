3e959e4bb6685a72db41e9dfd1cd937faeef305b
Seoyoung <seoyoung22dev@gmail.com>
2022-12-25 01:21:43 -0800

    Practice

--- print_practice.py
+++ print_practice.py
@@ -1,5 +1,76 @@
-# print 함수는 괄호 () 안의 식을 계산한 다음, 그 결과를 터미널에 출력합니다. 
-print(1 + 3)
+# print 
+print(1 + 1)
 
-# 숫자가 아닌 텍스트(문자열)를 표현하고 싶다면 '' 혹은 "" 로 감싸주어야 합니다. 
+# Text '' or ""
 print('HELLO WORKS!')
+
+# Variable
+result = 'HELLO WORKS!'
+print(result)
+
+result = 1 + 2 + 3 + 4 + 5
+print(result)
+
+result = result * 10 - 5
+print(result)
+
+#input / f-string
+name = input('What is your name?:')
+print(name)
+
+print(f'{name}, Hello! :)')
+
+# integer/ float
+print(1 + 2)  # 정수 더하기
+
+print(2 - 3)  # 정수 빼기
+
+print(3 * 4)  # 정수 곱하기
+
+print(4 / 5)  # 정수 나누기
+
+print(5 // 6)  # 정수 나눗셈의 몪
+
+print(6 % 7)  # 정수 나눗셈의 나머지
+
+print(7 ** 8)   # 정수 거듭제곱
+
+# String
+print('seoyoung') # 작은 따옴표
+
+print("SEOYOUNG") # 큰 따옴표
+
+print('Hello\nPython!') # 개행문자를 이용한 줄바꿈
+
+print('Hello' + 'WORLD!') # 문자열 덧셈
+
+print('Hello' * 3) # 문자열 곱셈
+
+print(len('SEOYOUNG')) # len함수로 문자열의 길이 계산
+
+
+# String slicing
+print('world'[0])
+
+print('world'[1])
+
+print('world'[4])
+
+print('world'[-1])
+
+print('world'[-2])
+
+print('skyblue'[0:3])
+
+# True/False
+print(1 == 1)
+
+print(1 != 1)
+
+print(1 < 1)
+
+print(1 > 1)
+
+print(1 <= 1)
+
+print(1 >=1)
